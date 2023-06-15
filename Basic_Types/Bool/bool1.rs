5761804b9086aa0381a674b03654acc61d4e858e
Ramesh Vyas <rameshovyas@gmail.com>
2023-06-15 22:23:50 +0530

    Exercise 1 in Bool section added

---
+++ Basic_Types/Bool/bool1.rs
@@ -0,0 +1,22 @@
+
+// Make println! work
+/*fn main() {
+    let _f: bool = false;
+
+    let t = true;
+    if !t {
+        println!("Success!");
+    }
+} */
+
+
+// Make println! work
+fn main() {
+    let _f: bool = false;
+
+    let t = false;
+    if !t {
+        println!("Success!");
+    }
+} 
+
