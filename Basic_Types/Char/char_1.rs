4775968a8318e1ea2f328281666b374f9bdf0cfd
Ramesh Vyas <rameshovyas@gmail.com>
2023-06-15 22:17:48 +0530

    exercise 1 in Char section added

---
+++ Basic_Types/Char/char_1.rs
@@ -0,0 +1,26 @@
+
+// Make it work
+/*use std::mem::size_of_val;
+fn main() {
+    let c1 = 'a';
+    assert_eq!(size_of_val(&c1),1); 
+
+    let c2 = '中';
+    assert_eq!(size_of_val(&c2),3); 
+
+    println!("Success!");
+} */
+
+
+// Make it work
+use std::mem::size_of_val;
+fn main() {
+    let c1 = 'a';
+    assert_eq!(size_of_val(&c1),4); //sizeof char is 4 bytes
+
+    let c2 = '中';
+    assert_eq!(size_of_val(&c2),4); //sizeof char is 4 bytes
+
+    println!("Success!");
+} 
+
