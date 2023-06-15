2efa3cf9d5d368406eeec8ec68622238cc474641
Ramesh Vyas <rameshovyas@gmail.com>
2023-06-15 22:20:15 +0530

    added exercise 2 in char section

---
+++ Basic_Types/Char/char_2.rs
@@ -0,0 +1,23 @@
+
+// Make it work
+/*fn main() {
+    let c1 = "中";
+    print_char(c1);
+} 
+
+fn print_char(c : char) {
+    println!("{}", c);
+}*/
+
+
+
+// Make it work
+fn main() {
+    let c1 = '中';  //character constant in single quotes not in double
+    print_char(c1);
+} 
+
+fn print_char(c : char) {
+    println!("{}", c);
+}
+
