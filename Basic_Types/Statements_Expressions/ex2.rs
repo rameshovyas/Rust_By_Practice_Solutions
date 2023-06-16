30b44c7b3b550eabc275c79d1c8cc99f0d572c43
Ramesh Vyas <rameshovyas@gmail.com>
2023-06-16 07:22:43 +0530

    exercise 2 of expressions section added

---
+++ Basic_Types/Statements_Expressions/ex2.rs
@@ -0,0 +1,19 @@
+
+/*fn main() {
+    let v = (let x = 3);
+ 
+    assert!(v == 3);
+ 
+    println!("Success!");
+ }
+ */
+
+ 
+fn main() {
+    let v = {let x = 3; x};
+ 
+    assert!(v == 3);
+ 
+    println!("Success!");
+ }
+ 
