8bec4d948553b7279ed92fc3c877de130e69a73e
Ramesh Vyas <rameshovyas@gmail.com>
2023-06-16 23:06:23 +0530

    ex9 ownership added

---
+++ Ownership_Borrowing/Ownership/ex9.rs
@@ -0,0 +1,21 @@
+/*
+fn main() {
+    let t = (String::from("hello"), String::from("world"));
+ 
+     // Fill the blanks
+     let (__, __) = __;
+ 
+     println!("{:?}, {:?}, {:?}", s1, s2, t); // -> "hello", "world", ("hello", "world")
+ }*/
+
+ 
+fn main() {
+    let t = (String::from("hello"), String::from("world"));
+ 
+     // Fill the blanks
+     let (ref s1, ref s2) = t;
+ 
+     println!("{:?}, {:?}, {:?}", s1, s2, t); // -> "hello", "world", ("hello", "world")
+ }
+ 
+ 
