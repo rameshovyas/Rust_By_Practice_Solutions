fd7a0a45378a8da543cdd2351c61fe7acafc2954
Ramesh Vyas <rameshovyas@gmail.com>
2023-06-16 08:36:09 +0530

    ex2 in functions added

---
+++ Basic_Types/Functions/ex2.rs
@@ -0,0 +1,19 @@
+/*fn main() {
+    print();
+ }
+ 
+ // Replace i32 with another type
+ fn print() -> i32 {
+    println!("Success!");
+ }*/
+
+ fn main() {
+    print();
+ }
+ 
+ // Returned unit ()
+ fn print() -> (){
+    println!("Success!");
+ }
+ 
+ 
