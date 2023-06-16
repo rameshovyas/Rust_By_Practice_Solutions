4a9178e515d432a3c11a99d2ad282577b5215244
Ramesh Vyas <rameshovyas@gmail.com>
2023-06-16 08:10:29 +0530

    exercise 3 in statement section added

---
+++ Basic_Types/Statements_Expressions/ex3.rs
@@ -0,0 +1,24 @@
+
+/*fn main() {
+    let s = sum(1 , 2);
+    assert_eq!(s, 3);
+
+    println!("Success!");
+}
+
+fn sum(x: i32, y: i32) -> i32 {
+    x + y;
+}*/
+
+
+fn main() {
+    let s = sum(1 , 2);
+    assert_eq!(s, 3);
+
+    println!("Success!");
+}
+
+fn sum(x: i32, y: i32) -> i32 {
+    x + y           // Removed semi colon to return otherwise it will return unit type ()
+}
+
