791fb17ac38785009ad541cf4c0dc152b97cb88c
Ramesh Vyas <rameshovyas@gmail.com>
2023-06-16 09:29:59 +0530

    ex4 in functions section added

---
+++ Basic_Types/Functions/ex4.rs
@@ -0,0 +1,55 @@
+/*
+fn main() {
+    println!("Success!");
+}
+
+fn get_option(tp: u8) -> Option<i32> {
+    match tp {
+        1 => {
+            // TODO
+        }
+        _ => {
+            // TODO
+        }
+    };
+    
+    // Rather than returning a None, we use a diverging function instead
+    never_return_fn()
+}
+
+// IMPLEMENT this function in THREE ways
+fn never_return_fn() -> ! {
+    
+}
+*/
+
+
+fn main() {
+    println!("Success!");
+}
+
+fn get_option(tp: u8) -> Option<i32> {
+    match tp {
+        1 => {
+            // TODO
+        }
+        _ => {
+            // TODO
+        }
+    };
+    
+    // Rather than returning a None, we use a diverging function instead
+    never_return_fn()
+}
+
+// IMPLEMENT this function in THREE ways
+fn never_return_fn() -> ! {
+   //First way 
+   //panic!() 
+
+   //second way 
+   //todo!()
+
+   //third way
+   unimplemented!()
+}
