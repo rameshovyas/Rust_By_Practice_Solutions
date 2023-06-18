c7ccf6002efcf42c65e114502694158d44b8aef0
Ramesh Vyas <rameshovyas@gmail.com>
2023-06-18 19:34:40 +0530

    ex1 in string section added

---
+++ Compound_Types/String/ex1.rs
@@ -0,0 +1,17 @@
+/*
+
+// Fix error without adding new line
+fn main() {
+    let s: str = "hello, world";
+
+    println!("Success!");
+}
+
+*/
+
+// Fix error without adding new line
+fn main() {
+    let s: &str = "hello, world";  // string literal refrence must be used
+
+    println!("Success!");
+}
