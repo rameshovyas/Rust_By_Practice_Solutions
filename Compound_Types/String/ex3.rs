8b6103f34a8e9653dbd8e04e0e1e2f0d435a5f6d
Ramesh Vyas <rameshovyas@gmail.com>
2023-06-20 22:08:53 +0530

    ex3 string section added

---
+++ Compound_Types/String/ex3.rs
@@ -0,0 +1,27 @@
+/*
+
+
+// Fill the blank
+fn main() {
+    let mut s = __;
+    s.push_str("hello, world");
+    s.push('!');
+
+    assert_eq!(s, "hello, world!");
+
+    println!("Success!");
+}
+
+*/
+
+
+// Fill the blank
+fn main() {
+    let mut s = String::from("");
+    s.push_str("hello, world");
+    s.push('!');
+
+    assert_eq!(s, "hello, world!");
+
+    println!("Success!");
+}
