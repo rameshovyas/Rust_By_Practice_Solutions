e6a91dca402c3c2d5c6ade9fdd76953accb47fdb
Ramesh Vyas <rameshovyas@gmail.com>
2023-06-20 22:06:22 +0530

    ex2 string section added

---
+++ Compound_Types/String/ex2.rs
@@ -0,0 +1,24 @@
+/*
+
+// Fix the error with at least two solutions
+fn main() {
+    let s: Box<str> = "hello, world".into();
+    greetings(s)
+}
+
+fn greetings(s: &str) {
+    println!("{}",s)
+}
+
+*/
+
+
+// Fix the error with at least two solutions
+fn main() {
+    let s: Box<str> = "hello, world".into();
+    greetings(&s)
+}
+
+fn greetings(s: &str) {
+    println!("{}",s)
+}
